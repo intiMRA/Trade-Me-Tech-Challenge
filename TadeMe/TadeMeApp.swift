db02a4487f4c3e3d38c97818ee4b701e6f068ee4
inti <inti.resende@gmail.com>
2022-01-21 16:20:09 +1300

    Initial Commit

---
+++ TadeMe/TadeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TadeMeApp.swift
+//  TadeMe
+//
+//  Created by Inti Albuquerque on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TadeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
